aefd0d067c3dd2c22869ae66838afb3001ad1715
Jhoss Sierra <joseangel184@gmail.com>
2023-09-21 18:45:19 -0400

    estilizando el boton

--- src/components/Goupbutton/GoUpButton.jsx
+++ src/components/Goupbutton/GoUpButton.jsx
@@ -6,20 +6,20 @@ import { useState } from 'react'
 
 export const GoUpButton = () => {
 
-const [scroll,setScroll] = useState(0)
-const []=useState()
+/* const [scroll,setScroll] = useState(0) */
+const [hide, setHide]=useState('false')
 
 window.addEventListener('scroll',()=>{
-    setScroll(window.scrollY)  
+    /* setScroll(window.scrollY); */
+    (window.scrollY>0)? setHide('true'): setHide('false') ;
 })
 
-if (scroll>0){
     return (
-        <a className='' href="#title"><button>
+        <a className={hide} href="#title"><button className={hide}>
         
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.2" stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
     </svg>
     
-    </button></a>)}
+    </button></a>)
 }
